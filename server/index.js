fcf154fc7ea941154712b96c4b314b3f7f1e815f
Nikita Kolesnew <chattrain.kol7@gmail.com>
2023-08-31 11:53:15 +0300

    connected to the database

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
-// import mongoose from "mongoose"; 
+import mongoose from "mongoose"; 
 import express from "express";
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 const PORT = process.env.PORT || 5000;
+const username = process.env.USERNAME_DB;
+const password = process.env.PASSWORD_DB;
+const DB_URL = `mongodb+srv://${username}:${password}@cluster.txpnkmr.mongodb.net/?retryWrites=true&w=majority`
 const app = express();
 
 
-const startApp = () => {
+const startApp = async () => {
     try {
         app.listen(PORT, () => console.log(`Server started on ${PORT}, http://localhost:${PORT}`));
+        await mongoose.connect(DB_URL);
     } catch (error) {
         console.log(error)
     }
